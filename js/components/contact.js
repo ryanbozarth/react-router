246cb3471892cf4b6c1cefab10cde38eb39087f1
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-02-01 21:42:03 -0800

    refacorting components

---
+++ js/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact(props) {
+    return (
+        <div>
+            <strong>
+                {props.name}
+            </strong>
+            &nbsp;
+            {props.phoneNumber}
+        </div>
+    );
+};
